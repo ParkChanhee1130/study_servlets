28ec2e980fce60ba6a3f38d8d65914cef42d9819
ParkChanhee1130 <kkazza1130@gmail.com>
2022-12-14 12:41:29 +0900

    first commit

---
+++ src/main/java/com/mgnggg/study_servlets/StudyServletsApplication.java
@@ -0,0 +1,16 @@
+package com.mgnggg.study_servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+
+@SpringBootApplication
+public class StudyServletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyServletsApplication.class, args);
+	}
+
+}
