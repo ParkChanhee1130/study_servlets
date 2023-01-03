d69db8c480db36cd757265c5b22b9c837911b015
ParkChanhee1130 <kkazza1130@gmail.com>
2023-01-03 12:19:56 +0900

    cm

--- src/main/java/com/mgnggg/study_servlets/dao/PollWithDB.java
+++ src/main/java/com/mgnggg/study_servlets/dao/PollWithDB.java
@@ -6,47 +6,55 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class PollWithDB{
-    public ArrayList<HashMap> getAnswerList(String quesionsUid) throws SQLException{
+public class PollWithDB {
 
-        Commons commons = new Commons();
-        Statement statement = commons.getStatement();
-        String query = "SELECT ANSWERS.QUESTIONS_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE " +
-                        "FROM ANSWERS "+
-                        "INNER JOIN EXAMPLE_LIST "+
-                        "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID "+
-                        "WHERE QUESTIONS_UID = '"+quesionsUid+"' " +
-                        "ORDER BY QUESTIONS_UID ";
-        ResultSet resultSet = statement.executeQuery(query);
+  public ArrayList<HashMap> getAnswerList(String quesionsUid)
+    throws SQLException {
+    Commons commons = new Commons();
+    Statement statement = commons.getStatement();
+    String query =
+      "SELECT ANSWERS.QUESTIONS_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE " +
+      "FROM ANSWERS " +
+      "INNER JOIN EXAMPLE_LIST " +
+      "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID " +
+      "WHERE QUESTIONS_UID = '" +
+      quesionsUid +
+      "' " +
+      "ORDER BY QUESTIONS_UID ";
+    ResultSet resultSet = statement.executeQuery(query);
 
-        ArrayList<HashMap> answer_list = new ArrayList<HashMap>();
-        while(resultSet.next()){
-            HashMap<String, Object> answer = new HashMap<String, Object>();
-            answer.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
-            answer.put("ORDERS",resultSet.getInt("ORDERS"));
-            answer.put("EXAMPLE",resultSet.getString("EXAMPLE"));
+    ArrayList<HashMap> answer_list = new ArrayList<HashMap>();
+    while (resultSet.next()) {
+      HashMap<String, Object> answer = new HashMap<String, Object>();
+      answer.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
+      answer.put("ORDERS", resultSet.getInt("ORDERS"));
+      answer.put("EXAMPLE", resultSet.getString("EXAMPLE"));
 
-            answer_list.add(answer);
-        }
-        return answer_list;
+      answer_list.add(answer);
     }
-    public HashMap<String, Object> getQuestion(String questionsUid) throws SQLException{
+    return answer_list;
+  }
 
-        Commons commons = new Commons();
-        Statement statement = commons.getStatement();
+  public HashMap<String, Object> getQuestion(String questionsUid)
+    throws SQLException {
+    Commons commons = new Commons();
+    Statement statement = commons.getStatement();
 
-        String query = "SELECT * FROM QUESTIONS_LIST " +
-                        " WHERE QUESTIONS_UID = '"+questionsUid+"'";
-        
-        ResultSet resultSet = statement.executeQuery(query);
-        HashMap<String, Object> result = null;
-        while(resultSet.next()){
-            result = new HashMap<>();
-            result.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
-            result.put("QUESTIONS", resultSet.getString("QUESTIONS"));
-            result.put("ORDERS",resultSet.getInt("ORDERS"));
-        }
+    String query =
+      "SELECT * FROM QUESTIONS_LIST " +
+      " WHERE QUESTIONS_UID = '" +
+      questionsUid +
+      "'";
 
-        return result;
+    ResultSet resultSet = statement.executeQuery(query);
+    HashMap<String, Object> result = null;
+    while (resultSet.next()) {
+      result = new HashMap<>();
+      result.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
+      result.put("QUESTIONS", resultSet.getString("QUESTIONS"));
+      result.put("ORDERS", resultSet.getInt("ORDERS"));
     }
+
+    return result;
+  }
 }
--- src/main/java/com/mgnggg/study_servlets/servlets/PollDetailServlets.java
+++ src/main/java/com/mgnggg/study_servlets/servlets/PollDetailServlets.java
@@ -17,37 +17,43 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-// /polls/PollServlet?QUESTIONS_UID=Q1
-@WebServlet(urlPatterns = "/polls/PollServlet") // web.xml
+@WebServlet(urlPatterns = "polls/PollServlet?QUESTIONS_UID=Q1") // web.xml
 public class PollDetailServlets extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // input type
-        String questions_Uid = request.getParameter("QUESTIONS_UID");
 
-        // biz with DB and Class
-        PollWithDB pollWithDB = new PollWithDB();
-        HashMap<String, Object> question = null;
-        ArrayList<HashMap> answer_list = null;
-        try {
-            question = pollWithDB.getQuestion(questions_Uid);
-            System.out.println(question.get("QUESTIONS_UID"));
-            System.out.println(question.get("QUESTIONS"));
-            System.out.println(question.get("ORDERS"));
-            answer_list = pollWithDB.getAnswerList(questions_Uid);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        for(int i=0;i < answer_list.size();i++){
-            HashMap<String, Object>  answer = answer_list.get(i);
-            System.out.println(answer.get("ORDERS"));
-            System.out.println(answer.get("EXAMPLE"));
-        }
-        // output with html
-        request.setAttribute("question", question);
-        request.setAttribute("answer_list", answer_list);
-        
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/polls/details.jsp");
-        requestDispatcher.forward(request, response);
+  @Override
+  protected void doGet(
+    HttpServletRequest request,
+    HttpServletResponse response
+  )
+    throws ServletException, IOException {
+    // input type
+    String questions_Uid = request.getParameter("QUESTIONS_UID");
+
+    // biz with DB and Class
+    PollWithDB pollWithDB = new PollWithDB();
+    HashMap<String, Object> question = null;
+    ArrayList<HashMap> answer_list = null;
+    try {
+      question = pollWithDB.getQuestion(questions_Uid);
+      System.out.println(question.get("QUESTIONS_UID"));
+      System.out.println(question.get("QUESTIONS"));
+      System.out.println(question.get("ORDERS"));
+      answer_list = pollWithDB.getAnswerList(questions_Uid);
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+    for (int i = 0; i < answer_list.size(); i++) {
+      HashMap<String, Object> answer = answer_list.get(i);
+      System.out.println(answer.get("ORDERS"));
+      System.out.println(answer.get("EXAMPLE"));
+    }
+    // output with html
+    request.setAttribute("question", question);
+    request.setAttribute("answer_list", answer_list);
+
+    RequestDispatcher requestDispatcher = request.getRequestDispatcher(
+      "/polls/details.jsp"
+    );
+    requestDispatcher.forward(request, response);
+  }
 }
